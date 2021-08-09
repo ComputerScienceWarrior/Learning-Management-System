fa99018b0e72a44a32912fcfa402df5ce5588fc0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-09 16:07:59 -0700

    run rails generator to add slug to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_225911) do
+ActiveRecord::Schema.define(version: 2021_08_09_230705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_08_09_225911) do
     t.text "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
