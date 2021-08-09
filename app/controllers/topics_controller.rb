a9c8c08497e4abec2f5107ebcc8969aac69228f4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-09 12:30:04 -0700

    create index and show action. creeate #find_course_and_topic method and reference in before_action call.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,17 @@
 class TopicsController < ApplicationController
+
+    before_action :find_topic, only: [:show]
+    def index
+        @topic = Topic.all
+    end
+
+    def show
+    end
+
+    private 
+
+    def find_course_and_topic
+        @topic = Topic.find(params[:id])
+        @course = @topic.course
+    end
 end
