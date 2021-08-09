4ee3fc8b45fc856a40c17e831029f7b455259bb0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-09 12:37:55 -0700

    create index and show actions. create #find_video_and_topic method and reference in before_action call.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,18 @@
 class VideosController < ApplicationController
+
+    before_action :find_video_and_topic, only: [:show]
+
+    def index
+        @videos = @topic.video
+    end
+
+    def show
+    end
+
+    private
+
+    def find_video_and_topic
+        @video = Video.find(params[:id])
+        @topic = @video.topic
+    end
 end
