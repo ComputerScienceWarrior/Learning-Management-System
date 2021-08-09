3c97adcf3063c015549dede04d86a3d3622c59d0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-09 16:20:10 -0700

    Add friendly designation in model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     private 
 
     def find_user
-        @user = User.find(params[:id])
+        @user = User.friendly.find(params[:id])
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    extend FriendlyId
+    friendly_id :username, use: :slugged
 
     def full_name
         self.first_name + " " + self.last_name
