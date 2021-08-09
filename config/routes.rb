564157a7b50355c767dade36f61163f930f5d634
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-09 13:20:26 -0700

    create routes for static controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   root "static#home"
+  get "/about", to: "static#about"
+  get "/settings", to: "static#settings"
+  
   resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
 
   resources :courses, only: [:index, :show] do 
