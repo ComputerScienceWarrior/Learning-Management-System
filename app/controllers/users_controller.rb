ebaf59cfbdb1e9a4665a67d1fd96a28669c5f685
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-10 16:14:27 -0700

    Add logic to update user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,12 @@ class UsersController < ApplicationController
     end
 
     def update
+        @user.update(user_params)
+        if @user.valid?
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
     end
 
     def destroy
