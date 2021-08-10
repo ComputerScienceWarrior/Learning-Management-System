ace910f3f05d09f8ea53f1486134870daf305fcb
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-10 10:38:24 -0700

    Create user login form and 'create' action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show, :create, :edit, :update, :destroy]
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
 
     def show
     end
@@ -9,6 +9,13 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
     end
 
     def edit
@@ -24,6 +31,10 @@ class UsersController < ApplicationController
 
     def find_user
         @user = User.friendly.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
     end
 
 end
