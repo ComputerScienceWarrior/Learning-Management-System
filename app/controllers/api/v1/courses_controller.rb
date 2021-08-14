a80fd8b94a1dc1fbbcc5c14f0658a9c965fd0936
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-14 01:38:05 -0700

    Add 'index' and 'show' serializer actions

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,7 +1,15 @@
 module Api
     module V1
         class CoursesController < ApplicationController
-            
+            def index
+                courses = Course.all
+                render json: CourseSerializer.new(courses).serialized_json
+            end
+
+            def show
+                course = Course.friendly.find(params[:id])
+                render json: CourseSerializer.new(course).serialized_json
+            end
         end
     end
 end
