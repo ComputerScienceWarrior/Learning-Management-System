f149df596e286036eee3f20176c43f7356878c2f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-18 09:57:51 -0700

    create CourseControlelr with index, show, create destroy, and update actions. Create find_course, course_params and options private methods.

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,14 +1,54 @@
 module Api
     module V1
         class CoursesController < ApplicationController
+            before_action :find_course, only: [:show, :update, :destroy]
+
             def index
-                courses = Course.all
-                render json: CourseSerializer.new(courses).serialized_json
+                @courses = Course.all
+                render json: CourseSerializer.new(@courses, options).serialized_json
             end
 
             def show
-                course = Course.friendly.find(params[:id])
-                render json: CourseSerializer.new(course).serialized_json
+                render json: CourseSerializer.new(@course, options).serialized_json
+            end
+
+            def create
+                @course = Course.new(course_params)
+                if @course.save
+                    render json: CourseSerializer.new(@course, options).serialized_json
+                else 
+                    render json: { error: @course.errors.messages }, status: 422
+                end
+            end
+
+            def update
+                if @course.update
+                    render json: CourseSerializer.new(@course, options).serialized_json
+                else 
+                    render json: { error: @course.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                if @course.destroy
+                    head :no_content
+                else
+                    render json: { error: @course.errors.messages }, status: 422
+                end
+            end
+
+            private 
+
+            def find_course
+                @course = Course.friendly.find(params[:id])
+            end
+
+            def course_params
+                params.require(:course).permit(:title, :summary, :category)
+            end
+
+            def options
+                @options ||= { include: %i[topics]} 
             end
         end
     end
