2bfbb38e45452f4dd4c244e478850d559667eab4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-18 10:04:35 -0700

    create video api controlelr with index, show, create update and destroy actions. Add find_video and video_params private methods.

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -1,7 +1,51 @@
 module Api
     module V1
         class VideosController < ApplicationController
-            
+            before_action :find_video, only: [:show, :update, :destroy]
+
+            def index
+                @videos = video.all
+                render json: VideoSerializer.new(@videos).serialized_json
+            end
+
+            def show
+                render json: VideoSerializer.new(@video).serialized_json
+            end
+
+            def create
+                @video = video.new(video_params)
+                if @video.save
+                    render json: VideoSerializer.new(@video).serialized_json
+                else 
+                    render json: { error: @video.errors.messages }, status: 422
+                end
+            end
+
+            def update
+                if @video.update
+                    render json: VideoSerializer.new(@video).serialized_json
+                else 
+                    render json: { error: @video.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                if @video.destroy
+                    head :no_content
+                else
+                    render json: { error: @video.errors.messages }, status: 422
+                end
+            end
+
+            private 
+
+            def find_video
+                @video = Video.friendly.find(params[:id])
+            end
+
+            def video_params
+                params.require(:video).permit(:title, :caption, :summary, :video_url, :thumbnail_url, :slug, :topic_id)
+            end
         end
     end
 end
