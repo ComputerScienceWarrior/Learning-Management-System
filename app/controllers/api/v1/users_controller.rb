dc97567e51cfe9fbf00e51b81529a0fce9f7913b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-18 09:28:31 -0700

    create users api controller with index, show, create, update and destroy actions. Also create find_user and user_params private methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,51 @@
 module Api
     module V1
         class UsersController < ApplicationController
-            
+            before_action :find_user, only: [:show, :update, :destroy]
+
+            def index
+                @users = User.all
+                render json: UserSerializer.new(user).serialized_json
+            end
+
+            def show
+                render json: UserSerializer.new(user).serialized_json
+            end
+
+            def create
+                @user = User.new(user_params)
+                if @user.save
+                    render json: UserSerializer.new(@user).serialized_json
+                else 
+                    render json: { error: @user.errors.messages }, status: 422
+                end
+            end
+
+            def update
+                if @user.update
+                    render json: UserSerializer.new(@user).serialized_json
+                else 
+                    render json: { error: @user.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                if @user.destroy
+                    head :no_content
+                else
+                    render json: { error: @user.errors.messages }, status: 422
+                end
+            end
+
+            private 
+
+            def find_user
+                @user = User.friendly.find(params[:id])
+            end
+
+            def user_params
+                params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+            end
         end
     end
 end
