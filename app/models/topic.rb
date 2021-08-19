7ce78a26e3b83b55c2d5f49ecbb48df6c0d85b5f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-18 19:22:40 -0700

    re-run migration with belongs_to association declared

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
-    extend FriendlyId
-    friendly_id :title, use: :slugged
-    belongs_to :course
-    has_many :videos
-    accepts_nested_attributes_for :videos
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  belongs_to :course
+  has_many :videos
+  accepts_nested_attributes_for :videos
 end
