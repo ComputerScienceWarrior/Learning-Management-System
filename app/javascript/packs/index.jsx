189b644c0bcc5068cbea8c769375fb32345f02c1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-19 15:15:13 -0700

    change 'from' designation in App inport.

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,7 +5,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import App from '../components/app'
+import App from '../components/App'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 const Hello = props => (
