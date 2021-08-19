31445e2790c5f046d2361d05b178ec8680284809
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-19 15:08:40 -0700

    import useState, useEffect and create a requiest to axios to hit api and display on Courses index page.

--- app/javascript/components/Courses/Courses.js
+++ app/javascript/components/Courses/Courses.js
@@ -1,11 +1,31 @@
 // Course index page
-import React from 'react'
+import React, {useState, useEffect, Fragment}from 'react'
+import axios from 'axios'
 
 const Courses = () => {
+
+    const [courses, setCourses] = useState([])
+
+    useEffect(()=>{
+        //get all courses from API
+        //update arlines in our state
+        axios.get('/api/v1/courses.json')
+        .then(resp => {
+            setCourses(resp.data)
+        })
+        .catch(resp => console.log(resp))
+    }, [courses.length])
+
+    const list = courses.map (item =>{
+        return (<li key={item.id}>{item.title}</li>)
+    })
     return (
-        <div>
-            This is the  Courses#index page
-        </div>
+        <Fragment>
+            <div>
+                This is the Courses#index page
+            </div>
+            <ul>{list}</ul>
+        </Fragment>
     )
 }
 
