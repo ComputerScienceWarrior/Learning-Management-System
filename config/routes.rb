9b5066f1bc5aa28f12217a36c24a3cc9f2a619da
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-19 14:57:13 -0700

    Clean up Routes file

--- config/routes.rb
+++ config/routes.rb
@@ -14,23 +14,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # root "courses#index"
-  # get "/about", to: "static#about"
-  # get "/settings", to: "static#settings"
-
-  # get "/login", to: "sessions#new"
-  # post "/login", to: "sessions#create"
-  # get "logout", to: "sessions#destroy"
-
-  # resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
-
-  # resources :courses, only: [:index, :show] do 
-  #   resources :topics, only: [:index, :show]
-  # end
-
-  # resources :topics, only: [:index, :show] do 
-  #   resources :videos, only: [:index, :show]
-  # end
-
-  get '*path', to: "courses#index", via: :all
+  root "static#home"
+  get '*path', to: "static#home", via: :all
 end
